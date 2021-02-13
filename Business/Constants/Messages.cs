098200f7e024fe707e07a3f7715eb04c88d1dbb6
Adil Özdurdu <aozdurdu@outlook.com>
2021-02-14 00:00:28 +0300

    13 Feb

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Product name invalid";
+        public static string MaintenanceTime = "System in maintenance mode";
+        public static string ProductsListed = "Product listed";
+    }
+}
