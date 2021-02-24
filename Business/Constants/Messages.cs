e6ee9c4866e674a2c28523eb554973d99085f840
Adil Özdurdu <aozdurdu@outlook.com>
2021-02-25 00:08:29 +0300

    25 Feb end of class

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Product name invalid";
         public static string MaintenanceTime = "System in maintenance mode";
         public static string ProductsListed = "Product listed";
+        public static string ProductCountOfCategoryError = "Exceed the max number in the related category";
+        public static string ProductNameAlreadyExists = "The product name already exists in db";
+        public static string CategoryLimitExceded = "Category limit is exceded, new product couldn't be added.";
     }
 }
